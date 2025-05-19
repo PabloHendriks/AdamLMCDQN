8b8e1c06f5b98e6fc5560b49b8df2ea31957ef66
Pablo Hendriks <pablo.hendriks@gmail.com>
2025-05-19 18:07:11 +0200

    feat: main file to run agents

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from adamlmcdqn!")
+import yaml
+
+from agents.adamLMCdqn import main as adamLMCDQN
+from agents.egreedy import main as egreedy
+
+def run():
+    with open("configs/cartPole.yaml", "r") as f:
+        config = yaml.safe_load(f)
+
+    adamLMCDQN(config=config)
+    egreedy(config=config)
 
 
 if __name__ == "__main__":
-    main()
+    run()
